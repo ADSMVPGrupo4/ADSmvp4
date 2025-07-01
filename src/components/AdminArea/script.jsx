5162fdf778fd775084010a940ae444fcd44be9c4
Mateus <mateussantanna2001@outlook.com>
2025-07-01 16:17:44 -0300

    CRUD feito

--- src/components/AdminArea/script.jsx
+++ src/components/AdminArea/script.jsx
@@ -1,42 +1,63 @@
-import EventCard from "../EventCard/script";
 import { AdminAreaConteiner } from "./style";
 
 const AdminArea = ({ options, setOptions, adminStatus, events, setEvents }) => {
   const accessAdmin = () => {
     if (options === "Admin" && adminStatus === true) {
+      const handleDeleteEvent = (id) => {
+        const confirmDelete = window.confirm(
+          "Tem certeza que deseja excluir este evento?",
+        );
+        if (confirmDelete) {
+          const updatedEvents = events.filter((event) => event.id !== id);
+          setEvents(updatedEvents);
+        }
+      };
       return (
         <>
           <AdminAreaConteiner>
             <div>
-              <h1>Eventos</h1>
-
-              <p>Evento</p>
-              <p>Data</p>
-              <p>Horário</p>
-              <p>Local</p>
-              <p>Tipo de atividade</p>
-              <p>Imagem</p>
-              <p>Ações</p>
-            </div>
-
-            <div>
-              {events.map((event) => (
-                <EventCard
-                  key={event.id}
-                  image={event.image}
-                  title={event.title}
-                  description={event.description}
-                  date={event.date}
-                  time={event.time}
-                  local={event.local}
-                  activityType={event.activityType}
-                  status={event.status}
-                />
-              ))}
-            </div>
-            <div>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Evento</th>
+                    <th>Data</th>
+                    <th>Horário</th>
+                    <th>Local</th>
+                    <th>Tipo de atividade</th>
+                    <th>Imagem</th>
+                    <th>Status</th>
+                    <th class="centro">Ações</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {events.map((event) => (
+                    <tr key={event.id}>
+                      <td>{event.title}</td>
+                      <td>{event.date}</td>
+                      <td>{event.time}</td>
+                      <td>{event.local}</td>
+                      <td>{event.activityType}</td>
+                      <td>
+                        <img
+                          src={event.image}
+                          alt={event.title}
+                          style={{ width: "100px" }}
+                        />
+                      </td>
+                      <td>{event.status}</td>
+                      <td>
+                        <button onClick={() => setOptions("Atualizar")}>
+                          Editar
+                        </button>
+                        <button onClick={() => handleDeleteEvent(event.id)}>
+                          Excluir
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
               <button onClick={() => setOptions("Criar")}>Novo</button>
-              <button onClick={() => setOptions("Atualizar")}>Editar</button>
             </div>
           </AdminAreaConteiner>
         </>
